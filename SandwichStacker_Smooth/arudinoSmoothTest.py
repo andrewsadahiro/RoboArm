c68aba7dcd6b57dc1abd7042e8d2e73d1025478f
AndrewS <andrewsadahiro@outlook.com>
2025-11-02 13:07:29 -0800

    smoothmove update

--- SandwichStacker_Smooth/arudinoSmoothTest.py
+++ SandwichStacker_Smooth/arudinoSmoothTest.py
@@ -5,9 +5,9 @@ serial1 = serial.Serial('/dev/ttyUSB0', 9600)
 def reset():
     serial1.write(b'base,90\n')
     time.sleep(0.5)
-    serial1.write(b'elbow,90\n')
-    time.sleep(0.5)
     serial1.write(b'shoulder,140\n')
+    time.sleep(0.5)
+    serial1.write(b'elbow,90\n')
     time.sleep(0.5)
     serial1.write(b'wrist,100\n')
     time.sleep(0.5)
@@ -26,13 +26,13 @@ def pickup():
     #open hand
     serial1.write(b'hand,00\n')
     time.sleep(1)
+    #rotate wrist
+    serial1.write(b'wrist,30\n')
+    time.sleep(1)
     #shoulder + elbow reach down
     serial1.write(b'elbow,40\n')
     time.sleep(1)
     serial1.write(b'shoulder,80\n')
-    time.sleep(1)
-    #rotate wrist
-    serial1.write(b'wrist,30\n')
     time.sleep(1)
     #close hand
     serial1.write(b'hand,180\n')
@@ -46,15 +46,15 @@ def pickup():
     serial1.write(b'base,75\n')
     time.sleep(1)
     #reach back down
-    serial1.write(b'elbow,40\n')
+    serial1.write(b'elbow,50\n')
     time.sleep(1)
-    serial1.write(b'shoulder,100\n')
+    serial1.write(b'shoulder,110\n')
     time.sleep(1)
     #open hand
     serial1.write(b'hand,0\n')
     time.sleep(1)
     print("Pickup complete")
-    time.sleep(10)
+    time.sleep(5)
     #reset
     reset()
     
@@ -79,7 +79,7 @@ def pushaway():
     serial1.write(b'base,75\n')
     time.sleep(1)
     print("Pushaway complete")
-    time.sleep(10)
+    time.sleep(5)
     #reset
     reset()
 
